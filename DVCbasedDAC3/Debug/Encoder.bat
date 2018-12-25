7c084e584f4f16c48138ae0882b4fd8329af80c6
WHUTFuYincheng <fuyincheng@whut.edu.cn>
2018-12-25 14:13:50 +0800

    update

--- DVCbasedDAC3/Debug/Encoder.bat
+++ DVCbasedDAC3/Debug/Encoder.bat
@@ -1,5 +1,2 @@
-::Encoder 7 25 176 144 30 1 city_qcif.yuv wz_city.bin rec_city.yuv
-::Encoder 6 25 352 288 30 1 foreman_cif_25hz.yuv wz_cif.bin rec_cif.yuv
-::Encoder 3 34 176 144 50 1 hall_qcif.yuv wz.bin rec.yuv
-Encoder 5 20 176 144 50 1 coastguard_qcif.yuv wz.bin rec.yuv
+Encoder 7 25 176 144 150 1 soccer_qcif.yuv wz.bin rec.yuv
 pause
